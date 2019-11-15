a132afc83a37d6b94d1c564755eb01f2431760df
JP Grafeen <jpgrafeen@gmail.com>
2019-11-15 06:56:28 -0800

    init dev commit, minimal main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*  Fall 2019
+*   JP Grafeen
+*   Jordan B
+*   Kelly 
+*
+*   ECE (4|5)85 Final Project
+*/
+
+
+int main (int argc, char *argv[])
+{
+   
+   return 0;
+}
+
+// ---------------
+// EOF
+// ---------------
