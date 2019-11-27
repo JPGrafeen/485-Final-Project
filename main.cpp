f221488e486ebdf868c6c05cc663e6a0943fd5e8
Jordan Bergmann <jordanb716@gmail.com>
2019-11-27 14:30:38 -0800

    Add code to get file name from arg or request then open file w/test
    Resolves #6

--- main.cpp
+++ main.cpp
@@ -1,16 +1,45 @@
 /*  Fall 2019
 *   JP Grafeen
-*   Jordan B
+*   Jordan Bergmann
 *   Kelly 
 *
 *   ECE (4|5)85 Final Project
 */
 
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main (int argc, char *argv[])
 {
-   
-   return 0;
+	//Get test file
+	ifstream file;
+	string filename;
+	if (argc < 2){ //No filename provided.
+		cout << "Please enter filename:" << endl;
+		cin >> filename;
+	}
+	else{
+		filename = argv[1];
+	}
+	file.open(filename);
+	if (!file){
+		cout << "File open error!" << endl;
+		return 1;
+	}
+
+	//TEST print file
+	string first;
+	string second;
+	while(getline(file, first, ' ')){
+		getline(file, second);
+		cout << "First: " << first << " Second: " << second << endl;
+	}
+
+	return 0;
 }
 
 // ---------------
